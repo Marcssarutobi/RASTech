cd72f9c5f76a52d0cd2df60f601fdd67bc20f051
hstkbj <hstkbj@asiso.io>
2024-05-15 17:15:18 +0100

    ContactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function CreateContact(Request $request){
+        $this->validate($request,[
+            "name"=>"required",
+            "email"=>"required",
+            "subject"=>"required",
+            "message"=>"required",
+            "status"=>"required",
+        ]);
+        $ctn = Contact::create([
+            "name"=>$request->name,
+            "email"=>$request->email,
+            "subject"=>$request->subject,
+            "message"=>$request->message,
+            "status"=>$request->status,
+        ]);
+        return response()->json([
+            "NewCtn"=>$ctn
+        ]);
+    }
+
+    public function AllContact(){
+        $ctns = Contact::orderBy('id','desc')->get();
+        return response()->json([
+            "allCtns"=>$ctns
+        ]);
+    }
+
+    public function  GetContact($id) {
+        
+    }
 }
--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Newsletter;
 use Illuminate\Http\Request;
 
 class NewsletterController extends Controller
 {
-    //
+    public function CreateNew(Request $request){
+        $this->validate($request,[
+            "email" => "required"
+        ]);
+        $news = Newsletter::create([
+            "email"=>$request->email
+        ]);
+        return response()->json([
+            "New" => $news
+        ]);
+    }
+
+    public function AllNew(){
+        $news = Newsletter::orderBy('id','desc');
+        return response()->json([
+            "News"=>$news
+        ]);
+    }
+
+    public function GetNew($id){
+        $new = Newsletter::where('id',$id)->first();
+        return response()->json([
+            "new"=>$new
+        ]);
+    }
+
+    public function DelNew(Request $request){
+        $new = Newsletter::where('id',$request->id)->delete();
+        return response()->json([
+            "delNew"=>$new
+        ]);
+    }
 }
